de4f5857b82b2bf6dcc1bfc921eef816e315773a
Joe Flesh <josephflesh@gmail.com>
2011-10-10 21:49:55 -0500

    ooooooo, woody

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Humanweb::Application.routes.draw do
 
-  get "home/index", :as => 'root'
-  root :to => 'home#index'
+  #get "home/index", :as => 'root'
+  #root :to => 'home#index'
+  get 'portfolio/index', :as => 'root'
+  root :to => 'portfolio#index'
 
   get "team" => 'home#team', :as => :team
   get "contact" => 'home#contact', :as => :contact
