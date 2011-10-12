fd594f05c766a3ff310672e1a03b87152d0d407e
Joe Flesh <josephflesh@gmail.com>
2011-10-11 22:42:00 -0500

    added jquery UI, more typography improvements

---
+++ public/javascripts/humanweb.js
@@ -0,0 +1,25 @@
+/*********************
+ *    humanweb.js    *
+ *   WEBSITE GUNK    *
+ *********************/
+var humanweb = humanweb = window.humanweb = {};
+
+// Stuff do do upon document.ready
+humanweb.init = function() {
+
+  // In case study summaries, highlight stuff smoothly on mouseover
+  $('.project').hover(function(){
+    $(this).find('h1,h2').animate({ 'color' : '#d71f26' }, 200);
+  }, function(){
+    $(this).find('h1,h2').animate({ 'color' : '#222222' }, 200);
+  });
+
+  // Turn entire case study summary into link
+  $('.project').click(function(){
+    window.location.href = $(this).find('a').attr('href');
+  });
+  
+}
+
+
+$(document).ready(humanweb.init);
