346d4816be24341b778f4ebe673f8b5a2246b241
Joe Flesh <josephflesh@gmail.com>
2011-10-13 15:23:01 -0500

    sexy "read more" overlay

--- public/javascripts/humanweb.js
+++ public/javascripts/humanweb.js
@@ -7,11 +7,15 @@ var humanweb = humanweb = window.humanweb = {};
 // Stuff do do upon document.ready
 humanweb.init = function() {
 
+  $('.project .screenshot').append($('.image-overlay').clone(true));
+
   // In case study summaries, highlight stuff smoothly on mouseover
   $('.project').hover(function(){
     $(this).find('h1,h2').animate({ 'color' : '#d71f26' }, 200);
+    $(this).find('.image-overlay').fadeIn(200);
   }, function(){
     $(this).find('h1,h2').animate({ 'color' : '#222222' }, 200);
+    $(this).find('.image-overlay').fadeOut(200);
   });
 
   // Turn entire case study summary into link
