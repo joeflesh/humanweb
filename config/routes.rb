d12b79566c0de5b26a511a10502d2fad5c7e31e3
Joe Flesh <josephflesh@gmail.com>
2011-10-13 19:17:34 -0500

    polish polish polish

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Humanweb::Application.routes.draw do
 
-  #get "home/index", :as => 'root'
-  #root :to => 'home#index'
-  get 'portfolio/index', :as => 'root'
-  root :to => 'portfolio#index'
+  get "home/index", :as => 'root'
+  root :to => 'home#index'
 
   get "team" => 'home#team', :as => :team
   get "contact" => 'home#contact', :as => :contact
