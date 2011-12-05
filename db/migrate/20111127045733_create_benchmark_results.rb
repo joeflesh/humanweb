9177a6e5f2249319c20430e45c465b507ab12802
Joe Flesh <josephflesh@gmail.com>
2011-12-05 12:33:10 -0600

    fixed migration

--- db/migrate/20111127045733_create_benchmark_results.rb
+++ db/migrate/20111127045733_create_benchmark_results.rb
@@ -2,6 +2,10 @@ class CreateBenchmarkResults < ActiveRecord::Migration
   def self.up
     create_table :benchmark_results do |t|
 
+      t.text :raw_results
+      t.string :instance_type
+      t.string :ami_id
+
       t.timestamps
     end
   end
